00084caaa594f9684f5a79b23c361e8faf560a84
IlyaPodkolzin <pdk.04@mail.ru>
2020-10-07 18:53:24 +0300

    Задание 6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,40 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int m = scan.nextInt();
         int count = 0;
-        int a[][] = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < m; j++) {
-                    a[i][j] = count;
-                    count++;
-                }
+        int bottom_y = n, bottom_x = n, top_y = n, top_x = n;
+        int current_x = n, current_y = n;
+        int a[][] = new int[2 * n + 1][2 * n + 1];
+        while (!(current_x == 2 * n && current_y == 0)) {
+            while (current_y >= bottom_y && !(current_x == 2 * n && current_y == 0)) {
+                a[current_x][current_y] = count;
+                count++;
+                current_y--;
             }
-            else {
-                for (int j = m - 1; j > -1; j--) {
-                    a[i][j] = count;
-                    count++;
-                }
+            bottom_y = current_y;
+            while (current_x >= bottom_x && !(current_x == 2 * n && current_y == 0)) {
+                a[current_x][current_y] = count;
+                count++;
+                current_x--;
             }
+            bottom_x = current_x;
+            while (current_y <= top_y && !(current_x == 2 * n && current_y == 0)) {
+                a[current_x][current_y] = count;
+                count++;
+                current_y++;
+            }
+            top_y = current_y;
+            while (current_x <= top_x && !(current_x == 2 * n && current_y == 0)) {
+                a[current_x][current_y] = count;
+                count++;
+                current_x++;
+            }
+            top_x = current_x;
         }
-        for (int i = 0; i < n ; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.printf("%3d", a[i][j]);
+        a[2 * n][0] = count;
+        for (int i = 0; i < 2 * n + 1; i++) {
+            for (int j = 0; j < 2 * n + 1; j++) {
+                System.out.printf("%3d", a[j][i]);
             }
             System.out.println();
         }
