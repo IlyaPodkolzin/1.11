858abfddd5fc42c6e1dfca6e6356912476b95b79
IlyaPodkolzin <pdk.04@mail.ru>
2020-10-07 18:52:21 +0300

    Задание 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int a[][] = new int[n][n];
-        boolean flag = true;
+        int m = scan.nextInt();
+        int a[][] = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = scan.nextInt();
+            for (int j = 0; j < m; j++) {
+                a[i][j] = i * j;
             }
         }
-        for (int i = 0; i < n; i++) {
-            if (!flag) {
-                break;
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.printf("%4d", a[i][j]);
             }
-            for (int j = 1; j < n - i; j++) {
-                if (a[i][i + j] != a[i + j][i]) {
-                    flag = false;
-                    System.out.print("no");
-                }
-            }
-        }
-        if (flag) {
-            System.out.print("yes");
+            System.out.println();
         }
     }
 }
