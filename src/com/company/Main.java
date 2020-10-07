c738df54b3eec093ad356b77c1bb4ee5770d63d2
IlyaPodkolzin <pdk.04@mail.ru>
2020-10-07 18:52:49 +0300

    Задание 5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,15 +8,25 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int m = scan.nextInt();
+        int count = 0;
         int a[][] = new int[n][m];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                a[i][j] = i * j;
+            if (i % 2 == 0) {
+                for (int j = 0; j < m; j++) {
+                    a[i][j] = count;
+                    count++;
+                }
+            }
+            else {
+                for (int j = m - 1; j > -1; j--) {
+                    a[i][j] = count;
+                    count++;
+                }
             }
         }
         for (int i = 0; i < n ; i++) {
             for (int j = 0; j < m; j++) {
-                System.out.printf("%4d", a[i][j]);
+                System.out.printf("%3d", a[i][j]);
             }
             System.out.println();
         }
